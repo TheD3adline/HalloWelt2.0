69c8ccf41bb46544d6cb60055aad47afb79b5403
MichaelFessler-Desktop <michaelfessler@gmx.at>
2022-01-20 13:25:12 +0100

    Modul 1 - Java Masterkurs

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println("Hallo Welt, ich programmiere gerne");
+
+        //Das ist ein Hallo Welt Programm und ein einzeiliger Kommentar
+
+        /* Und
+           das
+           ist
+           ein
+           mehrzeiliger
+           Kommentar
+         */
+
+    }
+}
